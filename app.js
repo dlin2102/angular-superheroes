087dc7948d4b6149d782ca6935db8e2d3b4f32cb
Dan Lin <dlin2102@gmail.com>
2017-03-06 22:17:27 -0500

    completed 8

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let super911Data = [
   {name:"Professor X", phone:"703-485-5653",comicOrigin:"Marvel",image_url:"http://areyouthereason.com/wp-content/uploads/2015/05/james-mcavoy-charles-xavier-759x500.jpg"},
-  {name:"Superman", phone:"703-221-3456",comicOrigin:"Marvel",image_url:"https://s-media-cache-ak0.pinimg.com/236x/a2/0b/fb/a20bfbcf2adc9d2bc6e68c4b5651526c.jpg"},
+  {name:"Superman", phone:"703-221-3456",comicOrigin:"Marvel",image_url:"http://static.comicvine.com/uploads/original/11120/111205813/4845173-3672402818-habra.jpg"},
   {name:"Spiderman", phone:"703-122-6453",comicOrigin:"Marvel",image_url:"http://cdn.collider.com/wp-content/uploads/tobey-maguire-spider-man-image.jpg"},
   {name:"Quicksilver", phone:"703-122-6453",comicOrigin:"Marvel",image_url:"http://static.srcdn.com/wp-content/uploads/Evan-Peters-Teases-X-Men-Apocalypse-Quicksilver-Scene.jpg"}
 ]
